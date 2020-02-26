83f1b96f44e247123df0c24b0ff1b28479e9323f
Niklas Nilsson <nn222ia@student.lnu.se>
2020-02-26 19:04:16 +0100

    heroku deploy

--- api/routes/root.js
+++ api/routes/root.js
@@ -11,46 +11,46 @@ router.get('/', (req, res) => {
       version: VERSION,
       author: 'Niklas Nilsson',
       desc: 'Player agents can view, store, delete and update the players in their stall.',
-      authenticationDesc: 'Register agent, login and recieve a token to set in Authentication header to make requests.'
+      authenticationDesc: 'Register agent, login and recieve a token in playload to set in Authentication header to make requests.'
     },
     links: {
       self: {
-        url: `http://${req.headers.host}${req.originalUrl}`,
+        url: `${req.protocol}://${req.headers.host}${req.originalUrl}`,
         method: 'GET',
         desc: 'Api root'
       },
       agents: {
         register: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents/register`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents/register`,
           method: 'POST',
           desc: 'Register: { username: <username>, password: <password>, email: <email> } (JSON format)'
         },
         login: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents/login`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents/login`,
           method: 'POST',
           desc: 'Login: { username: <username>, password: <password> } (JSON format), response: Token in payload'
         },
         read: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents`,
           method: 'GET',
           desc: 'View all registered agents'
         },
         readSingle: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
           method: 'GET',
           desc: 'View single agent',
           access: 'Private',
           token: 'Required'
         },
         update: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
           method: 'PATCH',
           desc: 'Update: [ { propName: <property to change>, value: <newValue> } ], Add more objects in order to change muliple properties',
           access: 'Private',
           token: 'Required'
         },
         delete: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/agents/:id`,
           method: 'DELETE',
           desc: 'Delete user',
           access: 'Private',
@@ -59,33 +59,33 @@ router.get('/', (req, res) => {
       },
       players: {
         add: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/players`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/players`,
           method: 'POST',
           desc: 'Add: { name: <playerName>, origin: <origin>, position: <position>, club: <club>, contractTo: < ex. 2022-02-12> }',
           access: 'Private',
           token: 'Required'
         },
         read: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/players`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/players`,
           method: 'GET',
           desc: 'View all players'
         },
         readSingle: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
           method: 'GET',
           desc: 'View single player',
           access: 'Private',
           token: 'Required'
         },
         update: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
           method: 'PATCH',
           desc: 'Update: [ { propName: <property to change>, value: <newValue> } ], Add more objects in order to change muliple properties',
           access: 'Private',
           token: 'Required'
         },
         delete: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/players/:id`,
           method: 'DELETE',
           desc: 'Delete player',
           access: 'Private',
@@ -94,21 +94,21 @@ router.get('/', (req, res) => {
       },
       hooks: {
         add: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/hooks`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/hooks`,
           method: 'POST',
           desc: 'Add: { callbackUrl: <url> }',
           access: 'Private',
           token: 'Required'
         },
         read: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/hooks`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/hooks`,
           method: 'GET',
           desc: 'view all hooks',
           access: 'Private',
           token: 'Required'
         },
         delete: {
-          url: `http://${req.headers.host}${req.originalUrl}/${VERSION}/hooks/:id`,
+          url: `${req.protocol}://${req.headers.host}${req.originalUrl}/${VERSION}/hooks/:id`,
           method: 'DELETE',
           desc: 'Delete hook',
           access: 'Private',
